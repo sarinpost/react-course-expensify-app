4bcd0a53d6aae398eb85d54185e7f846766fa8ca
sarinpost <30475718+sarinpost@users.noreply.github.com>
2021-09-27 18:12:36 +0700

    [update] add 3rd party component 'airbnb date picker'

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,19 @@
 import React from "react";
+import moment from 'moment'
+import { SingleDatePicker } from "react-dates"
+import 'react-dates/lib/css/_datepicker.css'
+
+// const date = new Date();
+const now = moment()
+console.log(now.format('MMM Do, YYYY'))
 
 export default class ExpenseForm extends React.Component {
     state = {
         description: '',
-        amount: 0,
+        amount: '',
         note: '',
+        createAt: moment(),
+        calendaFocused: false
     }
     onDescriptionChange = (e) => {
         const description = e.target.value
@@ -12,14 +21,33 @@ export default class ExpenseForm extends React.Component {
     }
     onNoteChange = (e) => {
         const note = e.target.value
-        this.setState(() => ({ note}))
+        this.setState(() => ({ note }))
+    }
+    onAmountChange = (e) => {
+        const amount = e.target.value
+        if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+            this.setState(() => ({ amount }))
+        }
+    }
+    onDateChange = (createAt) => {
+        this.setState(() => ({ createAt }))
+    }
+    onFocusChange = (focused) => {
+        this.setState(() => ({ calendaFocused: focused }))
     }
     render() {
         return (
             <div>
                 <form>
                     <input type="text" placeholder="Description" autoFocus value={this.state.description} onChange={this.onDescriptionChange} />
-                    <input type="number" placeholder="Amount" />
+                    <input type="number" placeholder="Amount" value={this.state.amount} onChange={this.onAmountChange} />
+                    <SingleDatePicker
+                        date={this.state.createAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calendaFocused.focused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths={1}
+                    />
                     <textarea placeholder="Add a note for your expense" value={this.state.note} onChange={this.onNoteChange}>
                     </textarea>
                     <button>Add Expense</button>
